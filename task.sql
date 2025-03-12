024540695612fff6ec3ff599756162d00ed74edc
mira <mkyshchak@gmail.com>
2025-03-12 23:19:49 +0200

    'task.sql.2'

--- task.sql
+++ task.sql
@@ -37,4 +37,9 @@ CREATE INDEX idx_email ON Customers(Email);
 CREATE INDEX idx_name ON Products(Name);      
 CREATE INDEX idx_customer_id ON Orders(CustomerID);  
 CREATE INDEX idx_order_id ON OrderItems(OrderID);     
-CREATE INDEX idx_product_id ON OrderItems(ProductID); 
+CREATE INDEX idx_product_id ON OrderItems(ProductID);
+
+SHOW INDEXES FROM Customers;
+SHOW INDEXES FROM Products;
+SHOW INDEXES FROM Orders;
+SHOW INDEXES FROM OrderItems;
