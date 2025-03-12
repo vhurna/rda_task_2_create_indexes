3228e049f6344e485f1354d246815d3a10095cb0
mira <mkyshchak@gmail.com>
2025-03-12 23:03:50 +0200

    'task.sql.2'

--- task.sql
+++ task.sql
@@ -1,2 +1,45 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+CREATE DATABASE IF NOT EXISTS ShopDB;
+
+USE ShopDB;
+
+CREATE TABLE IF NOT EXISTS Products (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    Description VARCHAR(100),
+    Price DECIMAL(10, 2),
+    WarehouseAmount INT
+);
+
+CREATE TABLE IF NOT EXISTS Customers (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Address VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT NULL,  
+    Date DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS OrderItems (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT NULL,  
+    ProductID INT NULL,  
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
+
+
+CREATE INDEX idx_email ON Customers(Email);
+
+CREATE INDEX idx_name ON Products(Name);
+
+CREATE INDEX idx_customer_id ON Orders(CustomerID);
+
+CREATE INDEX idx_order_id ON OrderItems(OrderID);
+
+CREATE INDEX idx_product_id ON OrderItems(ProductID);
