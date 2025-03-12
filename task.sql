befa95481889f0322f71411dd126aca2ac3640b5
mira <mkyshchak@gmail.com>
2025-03-12 23:17:27 +0200

    'task.sql.2'

--- task.sql
+++ task.sql
@@ -33,13 +33,8 @@ CREATE TABLE IF NOT EXISTS OrderItems (
     FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE  
 );
 
-
-CREATE INDEX idx_email ON Customers(Email);
-
-CREATE INDEX idx_name ON Products(Name);
-
-CREATE INDEX idx_customer_id ON Orders(CustomerID);
-
-CREATE INDEX idx_order_id ON OrderItems(OrderID);
-
-CREATE INDEX idx_product_id ON OrderItems(ProductID);
+CREATE INDEX idx_email ON Customers(Email);  
+CREATE INDEX idx_name ON Products(Name);      
+CREATE INDEX idx_customer_id ON Orders(CustomerID);  
+CREATE INDEX idx_order_id ON OrderItems(OrderID);     
+CREATE INDEX idx_product_id ON OrderItems(ProductID); 
